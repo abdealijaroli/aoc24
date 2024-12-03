68b80eeea7704da7854f22ed7517ecbeec68cf34
abdealijaroli <abdealiburu03@gmail.com>
2024-12-03 09:32:40 +0530

    day1 solution added

--- 1/main.go
+++ 1/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
+func calcTotalDistance(list1, list2 []int) int {
+	sort.Ints(list1)
+	sort.Ints(list2)
+
+	totalDistance := 0
+	for i := 0; i < len(list1); i++ {
+		distance := int(math.Abs(float64(list1[i] - list2[i])))
+		totalDistance += distance
+	}
+
+	return totalDistance
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	var list1, list2 []int
+
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		if len(fields) != 2 {
+			continue
+		}
+
+		num1, _ := strconv.Atoi(fields[0])
+		num2, _ := strconv.Atoi(fields[1])
+
+		list1 = append(list1, num1)
+		list2 = append(list2, num2)
+	}
+
+	fmt.Println(calcTotalDistance(list1, list2))
 }

200b8d8351888dc2e53a64fce31572766b32603a
abdealijaroli <abdealiburu03@gmail.com>
2024-12-13 09:40:05 +0530

    day 11 done

--- 11/main.go
+++ 11/main.go
@@ -1,121 +1,73 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strconv"
-    "strings"
-    "sync"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-type cacheKey struct {
-    num   int
-    depth int
+type TransformResult struct {
+	count   int
+	pattern []int
 }
 
-var cache = make(map[cacheKey][]int)
-var cacheMutex sync.RWMutex
+var memo = make(map[string]TransformResult)
 
-func transformZero(n int) []int {
-    if n == 0 {
-        return []int{1}
-    }
-    return []int{n}
+func predictGrowth(n int, depth int) int {
+	key := fmt.Sprintf("%d_%d", n, depth)
+	if result, exists := memo[key]; exists {
+		return result.count
+	}
+
+	if depth == 0 {
+		return 1
+	}
+
+	var nextCount int
+	if n == 0 {
+		nextCount = predictGrowth(1, depth-1)
+	} else if len(strconv.Itoa(n))%2 == 0 {
+		s := strconv.Itoa(n)
+		mid := len(s) / 2
+		left, _ := strconv.Atoi(s[:mid])
+		right, _ := strconv.Atoi(s[mid:])
+		nextCount = predictGrowth(left, depth-1) + predictGrowth(right, depth-1)
+	} else {
+		nextCount = predictGrowth(n*2024, depth-1)
+	}
+
+	memo[key] = TransformResult{nextCount, nil}
+	return nextCount
 }
 
-func splitEvenDigits(n int) []int {
-    s := strconv.Itoa(n)
-    if len(s)%2 == 0 {
-        mid := len(s)/2
-        left, _ := strconv.Atoi(s[:mid])
-        right, _ := strconv.Atoi(s[mid:])
-        return []int{left, right}
-    }
-    return []int{n}
-}
-
-func multiplyBy2024(n int) []int {
-    return []int{n * 2024}
-}
-
-func transformWithCache(stone int, depth int) []int {
-    key := cacheKey{stone, depth}
-    
-    cacheMutex.RLock()
-    if result, ok := cache[key]; ok {
-        cacheMutex.RUnlock()
-        return result
-    }
-    cacheMutex.RUnlock()
-    
-    var result []int
-    if depth == 0 {
-        return []int{stone}
-    }
-    
-    if stone == 0 {
-        result = transformZero(stone)
-    } else if len(strconv.Itoa(stone))%2 == 0 {
-        result = splitEvenDigits(stone)
-    } else {
-        result = multiplyBy2024(stone)
-    }
-    
-    var nextResult []int
-    for _, n := range result {
-        nextResult = append(nextResult, transformWithCache(n, depth-1)...)
-    }
-    
-    cacheMutex.Lock()
-    cache[key] = nextResult
-    cacheMutex.Unlock()
-    
-    return nextResult
-}
-
-func transformParallel(stones []int, depth int) []int {
-    var wg sync.WaitGroup
-    resultChan := make(chan []int, len(stones))
-    
-    for _, stone := range stones {
-        wg.Add(1)
-        go func(s int) {
-            defer wg.Done()
-            resultChan <- transformWithCache(s, depth)
-        }(stone)
-    }
-    
-    go func() {
-        wg.Wait()
-        close(resultChan)
-    }()
-    
-    var result []int
-    for r := range resultChan {
-        result = append(result, r...)
-    }
-    return result
+func solve(nums []int, depth int) int {
+	total := 0
+	for _, n := range nums {
+		total += predictGrowth(n, depth)
+	}
+	return total
 }
 
 func main() {
-    file, err := os.Open("11/input.txt")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer file.Close()
+	file, err := os.Open("11/input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    var nums []int
+	scanner := bufio.NewScanner(file)
+	var nums []int
 
-    if scanner.Scan() {
-        line := scanner.Text()
-        for _, numStr := range strings.Fields(line) {
-            num, _ := strconv.Atoi(numStr)
-            nums = append(nums, num)
-        }
-    }
+	if scanner.Scan() {
+		for _, numStr := range strings.Fields(scanner.Text()) {
+			num, _ := strconv.Atoi(numStr)
+			nums = append(nums, num)
+		}
+	}
 
-    stones := transformParallel(nums, 25)  // 75
-    fmt.Println(len(stones))
+	result := solve(nums, 75)
+	fmt.Println(result)
 }
